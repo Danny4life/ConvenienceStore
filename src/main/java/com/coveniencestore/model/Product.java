1c96fdb99e5c3a5b1c9062f1240d216f03bc51d4
Segun Osiki <osiki.segun@gmail.com>
2023-01-06 16:36:50 +0100

    add fields and method to Product class

--- src/main/java/com/coveniencestore/model/Product.java
+++ src/main/java/com/coveniencestore/model/Product.java
@@ -1,4 +1,34 @@
 package com.coveniencestore.model;
 
+import com.coveniencestore.enums.ProductAvailability;
+import com.coveniencestore.enums.ProductCategory;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static com.coveniencestore.enums.ProductAvailability.AVAILABLE;
+import static com.coveniencestore.enums.ProductAvailability.OUT_OF_STOCK;
+
+@Data
+@NoArgsConstructor
 public class Product {
+    private String name;
+    private double price;
+    private int quantity;
+    private ProductAvailability productAvailability;
+    private ProductCategory productCategory;
+
+    public Product(String name, double price, int quantity, ProductCategory productCategory) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.productCategory = productCategory;
+    }
+
+    public void checkProductAvailability(){
+        if(this.getQuantity() == 0){
+            this.setProductAvailability(OUT_OF_STOCK);
+        }else {
+            this.setProductAvailability(AVAILABLE);
+        }
+    }
 }
