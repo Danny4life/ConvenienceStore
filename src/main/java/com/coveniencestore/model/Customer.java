5bbfd4e99c2d2aa11d7ec0e1d46b2f31c4e1c324
Segun Osiki <osiki.segun@gmail.com>
2023-01-07 15:48:15 +0100

    add applicant class

--- src/main/java/com/coveniencestore/model/Customer.java
+++ src/main/java/com/coveniencestore/model/Customer.java
@@ -1,4 +1,23 @@
 package com.coveniencestore.model;
 
+import com.coveniencestore.enums.Gender;
+
 public class Customer extends Person{
+
+    private final Cart cart;
+    private final Wallet wallet;
+
+    public Customer(String firstName, String lastName, String address, Gender gender, Cart cart, Wallet wallet) {
+        super(firstName, lastName, address, gender);
+        this.cart = cart;
+        this.wallet = wallet;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public Wallet getWallet() {
+        return wallet;
+    }
 }
