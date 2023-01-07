d4c8c11ec040b24ddc198fc683af3dc366fc75a6
Segun Osiki <osiki.segun@gmail.com>
2023-01-07 19:07:01 +0100

    add Applicant not applied exception class

--- src/main/java/com/coveniencestore/exception/AlreadyAppliedException.java
+++ src/main/java/com/coveniencestore/exception/AlreadyAppliedException.java
@@ -1,4 +1,9 @@
 package com.coveniencestore.exception;
 
 public class AlreadyAppliedException extends RuntimeException {
+
+    public AlreadyAppliedException(String message){
+        super(message);
+    }
+
 }
---
+++ src/main/java/com/coveniencestore/exception/ApplicantNotAppliedException.java
@@ -0,0 +1,7 @@
+package com.coveniencestore.exception;
+
+public class ApplicantNotAppliedException extends RuntimeException{
+    public ApplicantNotAppliedException(String message){
+        super(message);
+    }
+}
