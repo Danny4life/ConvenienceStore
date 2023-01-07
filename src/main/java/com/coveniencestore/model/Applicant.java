9bada1e261e6bbb82716325bf7a460621cbc16cc
Segun Osiki <osiki.segun@gmail.com>
2023-01-07 15:51:15 +0100

    add files

--- src/main/java/com/coveniencestore/model/Applicant.java
+++ src/main/java/com/coveniencestore/model/Applicant.java
@@ -1,4 +1,29 @@
 package com.coveniencestore.model;
 
+import com.coveniencestore.enums.Gender;
+import com.coveniencestore.enums.Qualification;
+
 public class Applicant extends Person{
+
+    private Qualification qualification;
+
+    public Applicant(String firstName, String lastName, String address, Gender gender, Qualification qualification) {
+        super(firstName, lastName, address, gender);
+        this.qualification = qualification;
+    }
+
+    public Qualification getQualification() {
+        return qualification;
+    }
+
+    @Override
+    public String toString() {
+        return "Applicant{" +
+                "firstName='" + super.getFirstName() + '\'' +
+                "lastName='" + super.getLastName() + '\'' +
+                "address='" + super.getAddress()+ '\'' +
+                "gender=" + super.getGender() +
+                "qualification=" + qualification +
+                '}';
+    }
 }
