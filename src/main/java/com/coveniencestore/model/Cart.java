ba8300152853e81866e587108439233684630e39
Segun Osiki <osiki.segun@gmail.com>
2023-01-07 15:44:08 +0100

    add customer class

--- src/main/java/com/coveniencestore/model/Cart.java
+++ src/main/java/com/coveniencestore/model/Cart.java
@@ -1,4 +1,26 @@
 package com.coveniencestore.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cart {
+
+    private Map<Product, Integer> productsInCart;
+    private double totalCostOfProductInCart;
+
+    public Cart() {
+        this.productsInCart = new HashMap<>();
+    }
+
+    public Map<Product, Integer> getProductsInCart() {
+        return productsInCart;
+    }
+
+    public double getTotalCostOfProductInCart() {
+        return totalCostOfProductInCart;
+    }
+
+    public void setTotalCostOfProductInCart(double totalCostOfProductInCart) {
+        this.totalCostOfProductInCart = totalCostOfProductInCart;
+    }
 }
