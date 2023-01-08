72335e331f104607b1d0ad9782b832272f8ea1e5
Segun Osiki <osiki.segun@gmail.com>
2023-01-08 19:12:17 +0100

    add application service  class

---
+++ src/main/java/com/coveniencestore/application/ApplicationImpl.java
@@ -0,0 +1,11 @@
+package com.coveniencestore.application;
+
+import com.coveniencestore.model.Applicant;
+import com.coveniencestore.model.Store;
+
+public class ApplicationImpl implements Application{
+    @Override
+    public void apply(Applicant applicant, Store store) {
+
+    }
+}
