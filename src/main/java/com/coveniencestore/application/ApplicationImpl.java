e42b921e83aa734b9deb5a532571074a33651df1
Segun Osiki <osiki.segun@gmail.com>
2023-01-08 19:22:48 +0100

    add logic to Application class

--- src/main/java/com/coveniencestore/application/ApplicationImpl.java
+++ src/main/java/com/coveniencestore/application/ApplicationImpl.java
@@ -1,11 +1,19 @@
 package com.coveniencestore.application;
 
+import com.coveniencestore.exception.AlreadyAppliedException;
 import com.coveniencestore.model.Applicant;
 import com.coveniencestore.model.Store;
 
 public class ApplicationImpl implements Application{
     @Override
     public void apply(Applicant applicant, Store store) {
-
+        if(!store.getApplicants().contains(applicant)){
+            store.getApplicants().add(applicant);
+            System.out.printf("Thank you %s, for applying for this position, we'd get back to you after reviewing your application %n",
+                    applicant.getFirstName()
+            );
+        }else{
+            throw new AlreadyAppliedException("You already applied for this position.");
+        }
     }
 }
